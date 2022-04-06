8637179b96fcc22db1848721571c42c6a24a6628
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-04-06 16:45:05 -0300

    requisito 3 feito

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => { 
+  if (typeof name === 'string' && typeof age === 'number') {
+    let paragraph = `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+     return paragraph;
+  } return undefined;
+};
 
 module.exports = vqv;
